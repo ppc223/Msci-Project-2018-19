59ab51687d4f5eb6aaead07b1ecef23f19817991
ryanyeung <ryany414@gmail.com>
2019-02-20 19:24:44 +0000

    added numerical implementation

--- implementation-cube-num.py
+++ implementation-cube-num.py
@@ -7,11 +7,11 @@ import numpy as np
 from funcs import *
 
 # Column Heads for data output
-columns=['Wnorm', 'WLN', 'gamma', 'r', 'xbound', 'xcount',
-    'ybound', 'ycount']
+columns=['Wnorm', 'WLN', 'gamma', 'r', 'nmean', 'Ndim','xbound', 'xcount',
+    'ybound', 'ycount', 'time1', 'time2']
 
 # Name datafile for output and if not existing create and add column headings
-datafile = 'cubicanalytic.csv'
+datafile = 'cubicnumerical.csv'
 if not os.path.isfile(datafile):
     dfinit = pd.DataFrame(columns=columns)
     with open(datafile, 'a') as f:
@@ -23,53 +23,77 @@ dnorm = []
 dWLN = []
 dgamma = []
 dr = []
+dnmean = []
+# dN = []
 dxbound = []
 # dxcount = []
 dybound = []
 # dycount = []
-# dN = []
-# dtime1 = []
-# dtime2 = []
+dtime1 = []
+dtime2 = []
 
 pool = mp.Pool(processes=8)
 
 splitcount = 10
-gammas = np.split(np.linspace(0.1, 0.5, 20), splitcount)
-rs = np.linspace(0.1,0.5, 20)
+gammas = np.split(np.linspace(0.05, 0.5, 20), splitcount)
+rs = np.linspace(0.05,0.5, 20)
 
 xcount = ycount = 400
 
+N = 200
+
 for r in rs:
     for i in range(0, len(gammas)):
-        results = [pool.apply(wlnanalytic, (gamma, r, 1e-5, xcount, ycount),
-            dict([])) for gamma in gammas[i]]
+        states = []
+        tempgamma = []
+        tempnmean = []
+        results1 = [[cubic(gamma, r, N), gamma] for gamma in gammas[i]]
 
-        dgamma = dgamma + list(gammas[i])
-        dxcount = [xcount,] * len(dgamma)
-        dycount = [ycount,] * len(dgamma)
+        for result in results1:
+            states.append(result[0][0])
+            tempnmean.append(result[0][1])
+            tempgamma.append(result[1])
 
-        for result in results:
-            dWLN.append(result[0])
-            dnorm.append(result[1])
-            dxbound.append(result[2])
-            dybound.append(result[3])
+
+        results2 = [[wln(state, 1e-5, xcount, ycount, maxdepth = 300),
+            tempgamma[i], tempnmean[i]] for i, state in enumerate(states)]
+
+        for result in results2:
+            dWLN.append(result[0][0])
+            dnorm.append(result[0][1])
+            dtime1.append(result[0][2])
+            dtime2.append(result[0][3])
+            dxbound.append(result[0][4])
+            dybound.append(result[0][5])
+            dgamma.append(result[1])
+            dnmean.append(result[2])
             dr.append(r)
 
-        data = list(zip(dnorm, dWLN, dgamma, dr, dxbound, dxcount,
-            dybound, dycount))
+        dxcount = [xcount,] * len(dgamma)
+        dycount = [ycount,] * len(dgamma)
+        dN = [N,] * len(dgamma)
+
+        data = list(zip(dnorm, dWLN, dgamma, dr, dnmean, dN, dxbound, dxcount,
+            dybound, dycount, dtime1, dtime2))
+        #,dN, dtime1, dtime2))
 
         df = pd.DataFrame(data, columns=columns)
-
+        #
+        # columns=['Wnorm', 'WLN', 'gamma', 'r', 'nmean', 'Ndim','xbound', 'xcount',
+        #     'ybound', 'ycount', 'time1', 'time2']
         with open(datafile, 'a') as f:
             df.to_csv(f, header=False, index=False)
             dnorm = []
             dWLN = []
             dgamma = []
             dr = []
+
             dxbound = []
             dxcount = []
             dybound = []
             dycount = []
+            dtime1 = []
+            dtime2 = []
             print('exported data')
 
 
